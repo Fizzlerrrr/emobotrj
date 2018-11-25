2780547b69bda51e500b7d7dc9ecb3a812ab87fc
RJ <mcrk@outlook.com>
2018-11-26 04:26:45 +0800

    sentiment test#1

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var previewRecognizer = new builder_cognitiveservices.QnAMakerRecognizer({
 
 var basicQnAMakerPreviewDialog = new builder_cognitiveservices.QnAMakerDialog({
     recognizers: [previewRecognizer],
-    defaultMessage: 'No match! Try changing the query terms!',
+    defaultMessage: '你说什么？？',
     qnaThreshold: 0.3
 }
 );
@@ -81,36 +81,36 @@ bot.dialog('/', //basicQnAMakerDialog);
         }
     ]);
 
-    // bot.dialog('/', [
-    //     function (session) {
-    //         const msg = session.message.text;
-    //         const request = require('request');
-    //         var JSONBody = { "documents": [{ "language": "en", "id": "string", "text": msg }] };
+    bot.dialog('sentiment', [
+        function (session) {
+            const msg = session.message.text;
+            const request = require('request');
+            var JSONBody = { "documents": [{ "language": "ch", "id": "string", "text": msg }] };
     
-    //         request.post({
-    //             headers: { 'content-type': 'application/json', 'Ocp-Apim-Subscription-Key': textAnalyticsAPIKey },
-    //             url: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
-    //             json: JSONBody
+            request.post({
+                headers: { 'content-type': 'application/json', 'Ocp-Apim-Subscription-Key': textAnalyticsAPIKey },
+                url: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
+                json: JSONBody
     
-    //         }, function (error, response, body) {
-    //             if (error) {
-    //                 session.send(error);
-    //             } else {
-    //                 const sentimentScore = body.documents[0].score;
-    //                 if (sentimentScore > .8) {
-    //                     session.send(":)");
-    //                 } else if (sentimentScore > .5) {
-    //                     session.send(":|");
-    //                 } else if (sentimentScore > .3) {
-    //                     session.send(":O");
-    //                 } else if (sentimentScore > .1) {
-    //                     session.send(":\\");
-    //                 } else {
-    //                     session.send(":(");
-    //                 }
-    //                 session.send("The sentiment score for your input was: " + body.documents[0].score);
-    //             }
-    //         });
-    //     }
-    // ]);
+            }, function (error, response, body) {
+                if (error) {
+                    session.send(error);
+                } else {
+                    const sentimentScore = body.documents[0].score;
+                    if (sentimentScore > .8) {
+                        session.send(":)");
+                    } else if (sentimentScore > .5) {
+                        session.send(":|");
+                    } else if (sentimentScore > .3) {
+                        session.send(":O");
+                    } else if (sentimentScore > .1) {
+                        session.send(":\\");
+                    } else {
+                        session.send(":(");
+                    }
+                    session.send("文本情感值: " + body.documents[0].score);
+                }
+            });
+        }
+    ]);
     
